1ba14c351257fb7c9727f4fea9532ff3f3f22021
Surajit Sadhukhan <surajit@localhost.localdomain>
2017-12-03 21:09:35 +0530

    start building login form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,7 +15,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private todo : FormGroup;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private formBuilder: FormBuilder) {
+
+      this.todo = this.formBuilder.group({
+        title: ['', Validators.required],
+        description: [''],
+      });
+  }
+
+  logForm(){
+    console.log(this.todo.value)
   }
 
   ionViewDidLoad() {
