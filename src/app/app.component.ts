824811be2932550eb01d17604b05c40a46fefaba
Surajit Sadhukhan <surajit@localhost.localdomain>
2017-12-03 21:03:46 +0530

    added in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { RegistationPage } from "../pages/registation/registation";
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
+import { LoginPage } from '../pages/login/login';
+import { CrudPage } from "../pages/crud/crud";
 @Component({
   templateUrl: 'app.html'
 })
@@ -22,7 +22,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Login', component: LoginPage },
+      { title: 'Registration', component: RegistationPage },
+      { title: 'CRUD', component: CrudPage }
     ];
 
   }
